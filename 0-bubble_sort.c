03da5af2837ae3a8d8ce4a123024ac4b8a11c09e
AbdullahSec <Talonkali@outlook.com>
2026-08-20 18:54:37 +0300

    fixed 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,6 +14,11 @@ void bubble_sort(int *array, size_t size)
 	size_t i,j;
 	int tmp = 0;
 
+	if (!array || size < 2)
+	{
+		return;
+	}
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - 1; j++)
@@ -22,6 +27,8 @@ void bubble_sort(int *array, size_t size)
 			tmp = array[j];
 			array[j] = array[j + 1];
 			array[j + 1] = tmp;
+
+			print_array(array, size);
 	    }
 
 	}
